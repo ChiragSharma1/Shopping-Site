14ad9f091858c94e76c67746e18103d82f6b7b7e
shinchan1king <74426693+shinchan1king@users.noreply.github.com>
2022-10-11 02:10:36 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","ssfd");
+$con = mysqli_connect("sql6.freesqldatabase.com","sql6525607","","IKR7hHlKpZ");
 // Check connection
 if (mysqli_connect_errno())
   {
